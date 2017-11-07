f1794d6ab2dbfc062648279c7af1ab59ef647b4a
koheiw <watanabe.kohei@gmail.com>
2017-11-07 09:46:59 +0000

    return is a statement in Python 3

--- wk7/tools.py
+++ wk7/tools.py
@@ -10,7 +10,7 @@ def running_sum(ls):
     E.g., running_sum([1, 2, 3]) returns [1, 3, 6].'''
     for i in range(1, len(ls)):
         ls[i] = ls[i - 1] + ls[i]
-    return(ls)
+    return ls
 
 if __name__ == '__main__':
     main()
